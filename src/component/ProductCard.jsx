ec825514d9d5d2d2d7fa4d8d83e600efe78c6ac5
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-10-15 14:46:23 +0700

    style: product card

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,21 +1,52 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import s from "./ProductCard.module.css";
-export function ProductCard({ src, alt, title, id }) {
-  const navigate = useNavigate();
-  function goToProduct() {
-    navigate(`/product/${id}`);
-  }
+export function ProductCard({ src, title, id, price }) {
   return (
-    <div className={""} onClick={goToProduct}>
-      <div className={s.img}>
-        <img src={src} alt={alt} />
-      </div>
-      <div className={s.description}>
-        <p className={s.title}>{title}</p>
-        <p className={s.price}>$4.99</p>
-      </div>
-    </div>
+    <NavCard className={""} to={`/product/${id}`}>
+      <ImgContainer className={s.img}>
+        <img src={src} alt={title} />
+      </ImgContainer>
+      <Description>
+        <OneLineTitle>{title}</OneLineTitle>
+        <Price>${price}</Price>
+        <p>V 3.99</p>
+      </Description>
+    </NavCard>
   );
 }
+
+const NavCard = styled(NavLink)`
+  font-family: Arial, Helvetica, sans-serif;
+  color: black;
+  border-radius: 1rem;
+  text-decoration: none;
+  box-shadow: 1px 1px 10px #ddd;
+`;
+
+const Description = styled.div`
+  margin: 0.5rem;
+`;
+
+const Price = styled.p`
+  font-weight: bold;
+  font-size: 1.2rem;
+`;
+
+const OneLineTitle = styled.p`
+  width: 250px;
+  font-size: 1.2rem;
+  text-overflow: ellipsis;
+  white-space: pre;
+  overflow: hidden;
+`;
+
+const ImgContainer = styled.div`
+  background: white;
+  border-radius: 1rem 1rem 0 0;
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+`;
 
 // TODO: style
