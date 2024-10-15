2991d07401285599fdb7914e128cee2ddaf42833
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-10-15 16:05:19 +0700

    style: set main navbar to be sticky

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,10 +1,28 @@
 import { NavLink } from "react-router-dom";
+import { Container } from "./Container";
 import styled from "styled-components";
+export function NavBar({ cart }) {
+  return (
+    <StyledNav>
+      <Logo to="/">
+        <h1>Shopping</h1>
+      </Logo>
+      <Navlink to="/">Home</Navlink>
+      <Navlink to="shop">Shop</Navlink>
+      <Navlink to="cart">Cart ({cart})</Navlink>
+    </StyledNav>
+  );
+}
 
-const Nav = styled.nav`
+const StyledNav = styled.nav`
   display: flex;
   gap: 1rem;
   align-items: center;
+  padding: 1rem 0;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  ${Container}
 `;
 
 const Logo = styled(NavLink)`
@@ -19,19 +37,6 @@ const Navlink = styled(NavLink)`
     text-decoration: underline;
   }
 `;
-
-export function NavBar({ cart }) {
-  return (
-    <Nav>
-      <Logo to="/">
-        <h1>Shopping</h1>
-      </Logo>
-      <Navlink to="/">Home</Navlink>
-      <Navlink to="shop">Shop</Navlink>
-      <Navlink to="cart">Cart ({cart})</Navlink>
-    </Nav>
-  );
-}
 
 function SearchBar() {
   return <input type="text" />;
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,16 +2,21 @@ import { Outlet } from "react-router-dom";
 import { NavBar } from "../component/NavBar";
 import s from "./layout.module.css";
 import { useState } from "react";
+import styled from "styled-components";
 
 function RootLayout() {
   const [cartItems, setCartItems] = useState([]);
   let numOfItems = cartItems.reduce((a, b) => a + b.quantity, 0);
   return (
-    <div>
+    <StyledRoot>
       <NavBar cart={numOfItems} />
       <Outlet context={{ setCartItems, cartItems }} />
-    </div>
+    </StyledRoot>
   );
 }
+
+const StyledRoot = styled.div`
+  position: relative;
+`;
 
 export default RootLayout;
