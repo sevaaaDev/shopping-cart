3b8ac5713505656e4585bd2a50f4a9e398498472
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-10-17 12:56:21 +0700

    style: category grid responsive

--- src/component/CategoryGrid.jsx
+++ src/component/CategoryGrid.jsx
@@ -3,23 +3,33 @@ import styled from "styled-components";
 
 export function CategoryGrid({ data }) {
   return (
-    <Grid>
-      {data.map((el) => (
-        <ImgCard key={el.id}>
-          <NavLink to={`shop/category/${el.category}`}>
-            <img src={el.src} alt={el.title} />
-          </NavLink>
-          <Text>{el.category}</Text>
-        </ImgCard>
-      ))}
-    </Grid>
+    <Wrapper>
+      <h2>Browse Category</h2>
+      <Grid>
+        {data.map((el) => (
+          <ImgCard key={el.id}>
+            <NavLink to={`shop/category/${el.category}`}>
+              <img src={el.src} alt={el.title} />
+            </NavLink>
+            <Text>{el.category}</Text>
+          </ImgCard>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 5rem 0;
+  h2 {
+    text-align: center;
+  }
+`;
 
 const ImgCard = styled.div`
   position: relative;
   background-color: white;
-  border: 1px solid black;
+  box-shadow: 1px 1px 10px #ddd;
   padding: 0.5rem;
   & img {
     width: 100%;
@@ -39,16 +49,35 @@ const Text = styled.p`
 `;
 
 const Grid = styled.div`
-  margin: 5rem auto;
+  &:hover ${ImgCard} {
+    opacity: 0.4;
+  }
+  &:hover ${ImgCard}:hover {
+    opacity: 1;
+    scale: 1.1;
+    translate: 0px 10px;
+    z-index: 99;
+  }
+  & > * {
+    pointer-events: auto;
+  }
+  pointer-events: none;
+  margin: 1rem auto;
+  padding: 1rem;
+  box-shadow: 1px 1px 10px inset #ddd;
   display: grid;
   gap: 1rem;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   grid-template-rows: 200px 200px;
+  width: 90%;
   max-width: 900px;
-  & > div:nth-child(4) {
-    grid-column: span 2;
-  }
-  & > div:nth-child(3) {
-    grid-row: span 2;
+  @media (width > 425px) {
+    grid-template-columns: repeat(3, 1fr);
+    & > div:nth-child(4) {
+      grid-column: span 2;
+    }
+    & > div:nth-child(3) {
+      grid-row: span 2;
+    }
   }
 `;
