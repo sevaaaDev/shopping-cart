d594e50db5b569edf72ce771de94fdf3e9af08b1
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-10-28 12:24:52 +0700

    style: cart component

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -21,7 +21,11 @@ export function CartPage({ cartItems, setCartItems }) {
           />
         ))}
       </List>
-      <div>total price:{cartItems.reduce((a, b) => a + b.totalPrice, 0)}</div>
+      <div>
+        Total: $
+        {Math.round(cartItems.reduce((a, b) => a + b.totalPrice, 0) * 100) /
+          100}
+      </div>
       <Checkout>Checkout</Checkout>
     </div>
   );
@@ -30,11 +34,19 @@ export function CartPage({ cartItems, setCartItems }) {
 const Checkout = styled.button`
   display: block;
   width: 100%;
+  border: none;
+  padding: 0.5rem;
+  font-size: 1rem;
+  font-weight: bold;
+  background-color: #00bfff;
+  margin-top: 0.5rem;
 `;
 
 const List = styled.ul`
   list-style-type: none;
   margin-bottom: 1rem;
+  max-height: 80vh;
+  overflow: auto;
 `;
 
 // TODO: add img
@@ -64,8 +76,15 @@ function ProductCartCard({ el, setCartItems, cartItems }) {
           <img src={el.image} alt="" />
         </ImgWrapper>
         <Content>
-          <p>{el.title}</p>
-          <Category>{el.category}</Category>
+          <Col>
+            <div>
+              <p>{el.title}</p>
+              <Category>{el.category}</Category>
+            </div>
+            <DeleteBtn onClick={deleteItem}>
+              <Trash />
+            </DeleteBtn>
+          </Col>
           <Col>
             <Quantity>
               <BtnQuantity
@@ -87,6 +106,26 @@ function ProductCartCard({ el, setCartItems, cartItems }) {
     </li>
   );
 }
+function Trash() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="red"
+      class="bi bi-trash"
+      viewBox="0 0 16 16"
+    >
+      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+    </svg>
+  );
+}
+
+const DeleteBtn = styled.button`
+  border: none;
+  background-color: transparent;
+`;
 
 // TODO: delete btn
 // TODO: checkout btn
@@ -127,4 +166,5 @@ const Col = styled.div`
   margin-top: 1rem;
   display: flex;
   justify-content: space-between;
+  align-items: start;
 `;
